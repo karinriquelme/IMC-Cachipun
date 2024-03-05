7a665cfa1f69ec757ca766dd5e30df66be132187
Karina <kari.rik.888@gmail.com>
2024-03-05 12:51:42 -0300

    solucion  del problema en Cachipun

--- cachipun.py
+++ cachipun.py
@@ -5,17 +5,22 @@ usuario=sys.argv[1]
 
 opciones=["piedra", "papel", "tijeras"]
 compu=random.choice(opciones)
-print(f"tu jugaste {usuario}")
-print(f"computador jugo {compu}")
+
 
 if usuario in opciones:
     if usuario ==compu:
         print("Empate")
+        print(f"tu jugaste {usuario}")
+        print(f"computador jugo {compu}")
     elif ( (usuario=="piedra" and compu=="tijeras") or 
         (usuario=="papel"and compu=="piedra")  or 
         (usuario=="tijeras" and compu=="papel" )):
         print("Ganaste!!!")
+        print(f"tu jugaste {usuario}")
+        print(f"computador jugo {compu}")
     else:
         print("perdiste")
+        print(f"tu jugaste {usuario}")
+        print(f"computador jugo {compu}")
 else:
     print("Argumento invalido, debe ser piedra, papel o tijeras")
